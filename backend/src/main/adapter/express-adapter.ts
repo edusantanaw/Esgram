a9008ca7c0982e112d3eb4e199e9d5d3873aff57
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-01-14 20:51:58 -0300

    feat: express-adapter

--- backend/src/main/adapter/express-adapter.ts
+++ backend/src/main/adapter/express-adapter.ts
@@ -1,5 +1,16 @@
+import { Request, Response } from "express";
+import { Controller } from "../../protocols/adapter/controller";
 
+const adapt = (method: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpResponse = await method.handle({
+      ...req.body,
+      ...req.params,
+      ...req.query,
+      image: req.file,
+    });
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+  };
+};
 
-export default () => {
-    
-}
+export default adapt;
