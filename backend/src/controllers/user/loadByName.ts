e2a43eade9066517e3d399620c3c0ba82db996b5
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-01-14 11:05:40 -0300

    refactor: load user by name controller

---
+++ backend/src/controllers/user/loadByName.ts
@@ -0,0 +1,22 @@
+import {
+  badRequest,
+  catchError,
+  notContent,
+  ok,
+} from "../../helpers/httpReponse";
+import { ILoadUserUsecase } from "../../protocols/usecases/user/load";
+
+export class LoadUserByName {
+  constructor(private readonly loadUserUsecase: ILoadUserUsecase) {}
+
+  async handle({ name }: { name: string }) {
+    try {
+      if (!name) return badRequest("Name is required!");
+      const users = await this.loadUserUsecase.loadByName(name);
+      if (users) return notContent("users");
+      return ok(users);
+    } catch (error) {
+      return catchError(error);
+    }
+  }
+}
--- backend/src/usecases/user/update.ts
+++ backend/src/usecases/user/update.ts
@@ -1,17 +1,17 @@
 import { IUserRepository, updateUser } from "../../protocols/repository/user";
 
-
 export class UpdateUseusecase {
-    constructor(private  readonly userRepository: IUserRepository){}
+  constructor(private readonly userRepository: IUserRepository) {}
 
-    async execute(data: updateUser){
-        const user = await this.userRepository.findByEmail(data.email)
-        if(!user) throw "User not found!"
-        if(user.email !== data.email){
-            const verifyEmail = await this.userRepository.findByEmail(data.email)
-            if(verifyEmail) throw "Email already being used!"
-        }
-        const updatedUser = await this.userRepository.update(data)
-        return updatedUser
+  async execute(data: updateUser) {
+    const user = await this.userRepository.findByEmail(data.email);
+    if (!user) throw "User not found!";
+    if (user.email !== data.email) {
+      const verifyEmail = await this.userRepository.findByEmail(data.email);
+      if (verifyEmail) throw "Email already being used!";
     }
+
+    const updatedUser = await this.userRepository.update(data);
+    return updatedUser;
+  }
 }
